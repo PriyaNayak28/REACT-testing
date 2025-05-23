6e07f8f98933ea8231cf89766c967f889a87b7eb
PriyaNayak28 <nayakpriya612@gmail.com>
2025-05-23 23:57:15 +0530

    understand react test structure

--- src/App.test.js
+++ src/App.test.js
@@ -11,4 +11,6 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  const linkPara = screen.getByText(/Hello World/i)
+  expect(linkPara).toBeInTheDocument();
 });
